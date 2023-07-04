a862b490bc156b7e4ab5f1c1c1422e7d1455a3b3
Sh0g0-1758 <shouryagoel10000@gmail.com>
2023-07-05 00:33:48 +0530

    ran prettier to format the code

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,7 +8,6 @@ require("hardhat-gas-reporter");
 require("hardhat-contract-sizer");
 require("dotenv").config();
 
-
 const sepolia_url = process.env.sepolia_rpc_url;
 const private_key = process.env.PRIVATE_KEY;
 const etherscanKey = process.env.etherscan_api_key;
@@ -19,19 +18,21 @@ module.exports = {
 	defaultNetwork: "hardhat",
 	networks: {
 		hardhat: {
-			blockConfirmations : 1,
+			blockConfirmations: 1,
 			chainId: 31337,
 		},
 		localhost: {
 			url: "HTTP://127.0.0.1:7545",
-			accounts: ["0xed27420d96e524a6ef18c5dfad068ec328d6f0fc3c930ce18887fb695de63d4d"],
-			blockConfirmations : 1,
+			accounts: [
+				"0xed27420d96e524a6ef18c5dfad068ec328d6f0fc3c930ce18887fb695de63d4d",
+			],
+			blockConfirmations: 1,
 			chainId: 1337,
 		},
 		sepolia: {
 			url: sepolia_url,
 			accounts: [private_key],
-			blockConfirmations : 6,
+			blockConfirmations: 6,
 			chainId: 11155111,
 		},
 	},
